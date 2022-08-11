4230f3b5f6edc35647546202bd76538744cc87d2
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-11 19:29:17 +0300

    resolvers added with track.

---
+++ server/src/resolvers.js
@@ -0,0 +1,17 @@
+const resolvers = {
+    Query: {
+        // returns an array of Tracks that will be used to populate
+        // the homepage grid of our web client
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        } // we reshape context into datasource
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }  //we reshape parent into authorId
+    }
+
+};
+
+module.exports = resolvers;
