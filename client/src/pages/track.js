10e0261494d5b1b43f584e2dc73fc6dbd056efcc
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-12 16:58:34 +0300

    track pages data filled.

---
+++ client/src/pages/track.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from '../components/track-detail';
+
+const GET_TRACK = gql`
+query GetTrack($trackId: ID!) {
+  track(id: $trackId) {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+    numberOfViews
+    modules {
+      id
+      title
+      length
+    }
+    description
+  }
+}
+`;
+
+
+const Track = ({ trackId }) => {
+    const { loading, error, data } = useQuery(GET_TRACK, {
+        variables: { trackId }
+    });
+
+    return (
+        <Layout>
+            <QueryResult error={error} loading={loading} data={data}>
+                {/* this is where our component displaying the data will go */}
+                <TrackDetail track={data?.track} />
+            </QueryResult>
+        </Layout>
+    );
+}
+
+
+export default Track;
