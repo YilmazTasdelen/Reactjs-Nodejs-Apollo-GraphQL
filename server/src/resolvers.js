e65351c4d3a13442a578edaf1bd36e7c93fb2c03
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-12 16:18:25 +0300

    resolver chain updated.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,12 +4,22 @@ const resolvers = {
         // the homepage grid of our web client
         tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
-        } // we reshape context into datasource
+        }, // we reshape context into datasource
+
+        // get a single track by ID, for the track page
+        track: (_, { id }, { dataSources }) => {
+            return dataSources.trackAPI.getTrack(id);
+        }, // we restructure the arg into id 
     },
     Track: {
         author: ({ authorId }, _, { dataSources }) => {
             return dataSources.trackAPI.getAuthor(authorId);
-        }  //we reshape parent into authorId
+        },  //we reshape parent into authorId
+
+        modules: ({ id }, _, { dataSources }) => {
+            return dataSources.trackAPI.getTrackModules(id);
+        }  //we reshape parent into id (track module)
+
     }
 
 };
--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,8 @@ const typeDefs = gql`
 type Query {
   "Get tracks array for homepage grid"
   tracksForHome: [Track!]!
+  "Fetch a specific track, provided a track's ID"
+  track(id: ID!): Track
 }
 
 "A track is a group of Modules that teaches about a specific topic"
