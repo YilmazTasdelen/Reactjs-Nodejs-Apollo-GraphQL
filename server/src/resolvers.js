87842c1f71c8059a9d9996b3c2a50e1c27c05b85
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-12 18:13:07 +0300

    incrementTrackViews mutation/resolver added.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -20,6 +20,30 @@ const resolvers = {
             return dataSources.trackAPI.getTrackModules(id);
         }  //we reshape parent into id (track module)
 
+    },
+    Mutation: {
+        // where our new resolver function will go
+        incrementTrackViews: async (_, { id }, { dataSources }) => {
+            // where we'll call the TrackAPI
+            try {
+                const track = await dataSources.trackAPI.incrementTrackViews(id);
+                return {
+                    code: 200,
+                    success: true,
+                    message: `Successfully incremented number of views for track ${id}`,
+                    track
+                };
+            } catch (err) {
+                return {
+                    code: err.extensions.response.status,
+                    success: false,
+                    message: err.extensions.response.body,
+                    track: null
+                };
+            }
+
+
+        },
     }
 
 };
